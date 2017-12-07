ff08ae708830a9ce7414a4a80ba476389494cf02
Hebert Porto <hebertporto@gmail.com>
2017-12-06 23:08:03 -0200

    feat: compoente tabela

---
+++ src/modules/home/components/TableNovels.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from 'material-ui/Table'
+import { RaisedButton } from 'material-ui'
+
+const styleButton = {
+  margin: 5
+}
+
+class TableNovels extends Component {
+  render () {
+    return (
+      <Table>
+        <TableHeader
+          displaySelectAll={false}
+          adjustForCheckbox={false}
+        >
+          <TableRow>
+            <TableHeaderColumn>Novel</TableHeaderColumn>
+            <TableHeaderColumn>Último Capitulo</TableHeaderColumn>
+            <TableHeaderColumn>Ações</TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody
+          displayRowCheckbox={false}
+        >
+          <TableRow>
+            <TableRowColumn>Tales of Demons and Gods</TableRowColumn>
+            <TableRowColumn> 370 - 6/12/2017</TableRowColumn>
+            <TableRowColumn>
+              <RaisedButton label="Default" style={styleButton} />
+              <RaisedButton label="Primary" primary style={styleButton} />
+            </TableRowColumn>
+          </TableRow>
+        </TableBody>
+      </Table>
+    )
+  }
+}
+
+export default TableNovels
--- src/modules/home/containers/Home.jsx
+++ src/modules/home/containers/Home.jsx
@@ -3,15 +3,8 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Row, Col } from 'react-flexbox-grid'
 import { Link } from 'react-router-dom'
-import {
-  Table,
-  TableBody,
-  TableHeader,
-  TableHeaderColumn,
-  TableRow,
-  TableRowColumn,
-} from 'material-ui/Table'
 
+import TableNovels from './../components/TableNovels'
 import SectionCentered from './../../shared/components/grid/SectionCentered'
 
 import If from './../../shared/components/helpers/If'
@@ -26,47 +19,14 @@ class Home extends Component {
   render () {
     return (
       <SectionCentered>
-        <Table>
-          <TableHeader
-            displaySelectAll={false}
-            adjustForCheckbox={false}
-          >
-            <TableRow>
-              <TableHeaderColumn>ID</TableHeaderColumn>
-              <TableHeaderColumn>Name</TableHeaderColumn>
-              <TableHeaderColumn>Status</TableHeaderColumn>
-            </TableRow>
-          </TableHeader>
-          <TableBody
-            displayRowCheckbox={false}
-          >
-            <TableRow>
-              <TableRowColumn>1</TableRowColumn>
-              <TableRowColumn>John Smith</TableRowColumn>
-              <TableRowColumn>Employed</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>2</TableRowColumn>
-              <TableRowColumn>Randal White</TableRowColumn>
-              <TableRowColumn>Unemployed</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>3</TableRowColumn>
-              <TableRowColumn>Stephanie Sanders</TableRowColumn>
-              <TableRowColumn>Employed</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>4</TableRowColumn>
-              <TableRowColumn>Steve Brown</TableRowColumn>
-              <TableRowColumn>Employed</TableRowColumn>
-            </TableRow>
-            <TableRow>
-              <TableRowColumn>5</TableRowColumn>
-              <TableRowColumn>Christopher Nolan</TableRowColumn>
-              <TableRowColumn>Unemployed</TableRowColumn>
-            </TableRow>
-          </TableBody>
-        </Table>
+        <Row>
+          <Col xs={12} md={8}>
+            <TableNovels />
+          </Col>
+          <Col xs={12} md={4}>
+            <span>Menu Lateral</span>
+          </Col>
+        </Row>
       </SectionCentered>
     )
   }
